a175b63db722dcbf2936425ba484bc8b42e82c44
gregory <gregoryarcherk@gmail.com>
2023-02-14 20:59:43 -0500

    experiment 1 added (finally)

--- experiment_1.py
+++ experiment_1.py
@@ -1,10 +1,31 @@
 import matplotlib.pyplot as plot
 from experiment_2 import average_probabilities
-from graph import Graph, has_cycle
+from graph import Graph, has_cycle, create_random_graph
 
 NODE_CASES = [5, 10, 25, 50]
 STEPS = 25
 AVGS = 500
+
+def cycle_probability(num_nodes, proportions, avgs):
+    maximum_edges = num_nodes
+    probabilities = []
+
+    for proportion in proportions:
+        edges = int(proportion / 100 * maximum_edges)
+        outcomes = []
+
+        for _ in range(avgs):
+            G = create_random_graph(num_nodes, edges)
+            if has_cycle(G):
+                outcomes.append(100)
+            else:
+                outcomes.append(0)
+
+        probabilities.append(outcomes)
+        print(f"Completed {edges} edges with {num_nodes} nodes ({proportion}%)")
+
+    return [sum(p) / avgs for p in probabilities]
+
 
 if __name__ == "__main__":
     proportions = list(range(0, 100, 100 // STEPS))
@@ -12,7 +33,7 @@ if __name__ == "__main__":
     for num_nodes in NODE_CASES:
         plot.plot(
             proportions,
-            average_probabilities(num_nodes, proportions, AVGS, has_cycle),
+            cycle_probability(num_nodes, proportions, AVGS),
             label=f"{num_nodes} nodes"
         )
 
