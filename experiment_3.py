5c82e72ff595d8d00085999ddbf1ac297df861ac
Gaurav Sharma <101910330+GauravS43@users.noreply.github.com>
2023-02-15 09:46:15 -0500

    updated experiment_3

--- experiment_3.py
+++ experiment_3.py
@@ -1,16 +1,18 @@
 # First experiment, tests sums of minimum covers with static 8 nodes and increasing edges
-from graph import create_random_graph, MVC, approx2
+from graph import create_random_graph, MVC, approx2, approx3
 import matplotlib.pyplot as plot
 
 mvc_sums = []
 approx1_sums = []
 approx2_sums = []
 approx3_sums = []
+runs = 15
 
-for m in range(10):
+
+for m in range(runs):
     graphs = []
     for i in range(1000):
-        g = create_random_graph(8, (m + 1) * 2)
+        g = create_random_graph(8, m * 2)
         graphs.append(g)
 
     mvc_sum = 0
@@ -28,12 +30,12 @@ for m in range(10):
     approx2_sums.append(a2_sum)
     approx3_sums.append(a3_sum)
 
-plot.plot([2 * (x + 1) for x in range(10)], mvc_sums, label="Minimum Vertex Cover")
+plot.plot([2 * x for x in range(runs)], mvc_sums, label="Minimum Vertex Cover")
 # plot.plot([2 * (x + 1) for x in range(10)], approx1_sums, label="Approximation 1")
-plot.plot([2 * (x + 1) for x in range(10)], approx2_sums, label="Approximation 2")
-plot.plot([2 * (x + 1) for x in range(10)], approx3_sums, label="Approximation 3")
+plot.plot([2 * x for x in range(runs)], approx2_sums, label="Approximation 2")
+plot.plot([2 * x for x in range(runs)], approx3_sums, label="Approximation 3")
 
-plot.table("Approximations vs Actual MVC")
+plot.title("Approximations vs Actual MVC")
 plot.xlabel("Graph Length")
 plot.ylabel("Length of List of Vertex Cover")
 plot.legend()
