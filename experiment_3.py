27ec3005dd8b2ceedfb8ede11ac166b997d36e21
gregory <gregoryarcherk@gmail.com>
2023-02-15 11:33:13 -0500

    added exp3 graph

--- experiment_3.py
+++ experiment_3.py
@@ -74,3 +74,42 @@ plot.xlabel("Number of Nodes")
 plot.ylabel("Expected Performance")
 plot.legend()
 plot.savefig("graphs/experiment_3_2.png")
+
+# Third approxximation experiment, tests sum of minimum covers with increasing num of nodes and proportional edges
+
+plot.clf()
+approx1_sums.clear()
+approx2_sums.clear()
+approx3_sums.clear()
+runs = 8
+
+for m in range(runs):
+    graphs = []
+    for i in range(1000):
+        g = create_random_graph(m + 4, (m + 4) / 2)
+        graphs.append(g)
+    
+    mvc_sum = 0
+    a1_sum  = 0
+    a2_sum  = 0
+    a3_sum  = 0
+
+    for g in graphs:
+        mvc_sum += len(MVC(g))
+        a1_sum += len(approx1(g))
+        a2_sum += len(approx2(g))
+        a3_sum += len(approx3(g))
+
+    approx1_sums.append(a1_sum / mvc_sum)
+    approx2_sums.append(a2_sum / mvc_sum)
+    approx3_sums.append(a3_sum / mvc_sum)
+
+plot.plot([x + 4 for x in range(runs)], approx1_sums, label="Approximation 1")
+plot.plot([x + 4 for x in range(runs)], approx2_sums, label="Approximation 2")
+plot.plot([x + 4 for x in range(runs)], approx3_sums, label="Approximation 3")
+
+plot.title("")
+plot.xlabel("Number of Nodes (with proportional edges)")
+plot.ylabel("Expected Performance")
+plot.legend()
+plot.savefig("graphs/experiment_3_3.png")
