116e30de3505f67cb56177c2fb9c532de050f0b6
Gaurav Sharma <101910330+GauravS43@users.noreply.github.com>
2023-02-15 10:11:52 -0500

    added approx 1 to experiment 3

--- experiment_3.py
+++ experiment_3.py
@@ -1,5 +1,5 @@
 # First approximation experiment, tests sums of minimum covers with static 8 nodes and increasing edges
-from graph import create_random_graph, MVC, approx2, approx3
+from graph import create_random_graph, MVC, approx1, approx2, approx3
 import matplotlib.pyplot as plot
 
 mvc_sums = []
@@ -20,17 +20,17 @@ for m in range(runs):
     a3_sum = 0
     for g in graphs:
         mvc_sum += len(MVC(g))
-        # a1_sum += len(approx1(g))
+        a1_sum += len(approx1(g))
         a2_sum += len(approx2(g))
         a3_sum += len(approx3(g))
 
     mvc_sums.append(mvc_sum)
-    # approx1_sums.append(a1_sum)
+    approx1_sums.append(a1_sum)
     approx2_sums.append(a2_sum)
     approx3_sums.append(a3_sum)
 
 plot.plot([2 * x for x in range(runs)], mvc_sums, label="Minimum Vertex Cover")
-# plot.plot([2 * (x + 1) for x in range(10)], approx1_sums, label="Approximation 1")
+plot.plot([2 * x for x in range(runs)], approx1_sums, label="Approximation 1")
 plot.plot([2 * x for x in range(runs)], approx2_sums, label="Approximation 2")
 plot.plot([2 * x for x in range(runs)], approx3_sums, label="Approximation 3")
 
@@ -52,7 +52,7 @@ runs = 8
 for m in range(runs):
     graphs = []
     for i in range(1000):
-        g = create_random_graph(m + 2, m + 1)
+        g = create_random_graph(m + 4, 6)
         graphs.append(g)
 
     mvc_sum = 0
@@ -61,19 +61,19 @@ for m in range(runs):
     a3_sum = 0
     for g in graphs:
         mvc_sum += len(MVC(g))
-        # a1_sum += len(approx1(g))
+        a1_sum += len(approx1(g))
         a2_sum += len(approx2(g))
         a3_sum += len(approx3(g))
 
     mvc_sums.append(mvc_sum)
-    # approx1_sums.append(a1_sum)
+    approx1_sums.append(a1_sum)
     approx2_sums.append(a2_sum)
     approx3_sums.append(a3_sum)
 
-plot.plot([x + 2 for x in range(runs)], mvc_sums, label="Minimum Vertex Cover")
-# plot.plot([2 * (x + 1) for x in range(10)], approx1_sums, label="Approximation 1")
-plot.plot([x + 2 for x in range(runs)], approx2_sums, label="Approximation 2")
-plot.plot([x + 2 for x in range(runs)], approx3_sums, label="Approximation 3")
+plot.plot([x + 4 for x in range(runs)], mvc_sums, label="Minimum Vertex Cover")
+plot.plot([x + 4 for x in range(runs)], approx1_sums, label="Approximation 1")
+plot.plot([x + 4 for x in range(runs)], approx2_sums, label="Approximation 2")
+plot.plot([x + 4 for x in range(runs)], approx3_sums, label="Approximation 3")
 
 plot.title("Approximations vs Actual MVC (2)")
 plot.xlabel("Number of Nodes")
