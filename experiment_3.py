bbf1e0665dff64fe6527c6c20d607e24e683f5e2
Gaurav Sharma <101910330+GauravS43@users.noreply.github.com>
2023-02-25 21:19:13 -0500

    Edited test cases for approx experiment 1 and 2

--- experiment_3.py
+++ experiment_3.py
@@ -1,16 +1,19 @@
-# First approximation experiment, tests sums of minimum covers with static 8 nodes and increasing edges
 from graph import create_random_graph, MVC, approx1, approx2, approx3
 import matplotlib.pyplot as plot
+
+# First approximation experiment, tests sums of approximations with static 8 nodes and increasing edges
+# First Test Case ~ 8 Nodes, 1 Edges
+# Final Test Case ~ 8 Nodes, 28 Edges
 
 approx1_sums = []
 approx2_sums = []
 approx3_sums = []
-runs = 15
+runs = 29
 
 for m in range(1, runs):
     graphs = []
     for i in range(1000):
-        g = create_random_graph(8, m * 2)
+        g = create_random_graph(8, m)
         graphs.append(g)
 
     mvc_sum = 0
@@ -27,28 +30,30 @@ for m in range(1, runs):
     approx2_sums.append(a2_sum / mvc_sum)
     approx3_sums.append(a3_sum / mvc_sum)
 
-plot.plot([2 * x for x in range(1, runs)], approx1_sums, label="Approximation 1")
-plot.plot([2 * x for x in range(1, runs)], approx2_sums, label="Approximation 2")
-plot.plot([2 * x for x in range(1, runs)], approx3_sums, label="Approximation 3")
+plot.plot([x for x in range(1, runs)], approx1_sums, label="Approximation 1")
+plot.plot([x for x in range(1, runs)], approx2_sums, label="Approximation 2")
+plot.plot([x for x in range(1, runs)], approx3_sums, label="Approximation 3")
 
-plot.title("Approximations vs Actual MVC (1)")
+plot.title("Expected Performance as Edges Increase")
 plot.xlabel("Number of Edges")
 plot.ylabel("Expected Performance")
 plot.legend()
 plot.savefig("graphs/experiment_3_1.png")
 
-# Second approximation experiment, tests sums of minimum covers with increasing num of nodes and 6 edges
+# Second approximation experiment, tests sums of approximations with increasing num of nodes and 6 edges
+# First Test Case ~ 4 Nodes, 6 Edges
+# Final Test Case ~ 15 Nodes, 6 Edges
 
 plot.clf()
 approx1_sums.clear()
 approx2_sums.clear()
 approx3_sums.clear()
-runs = 8
+runs = 16
 
-for m in range(runs):
+for m in range(4, runs):
     graphs = []
     for i in range(1000):
-        g = create_random_graph(m + 4, 6)
+        g = create_random_graph(m, 6)
         graphs.append(g)
 
     mvc_sum = 0
@@ -61,15 +66,16 @@ for m in range(runs):
         a2_sum += len(approx2(g))
         a3_sum += len(approx3(g))
 
+    print(m)
     approx1_sums.append(a1_sum / mvc_sum)
     approx2_sums.append(a2_sum / mvc_sum)
     approx3_sums.append(a3_sum / mvc_sum)
 
-plot.plot([x + 4 for x in range(runs)], approx1_sums, label="Approximation 1")
-plot.plot([x + 4 for x in range(runs)], approx2_sums, label="Approximation 2")
-plot.plot([x + 4 for x in range(runs)], approx3_sums, label="Approximation 3")
+plot.plot([x for x in range(4, runs)], approx1_sums, label="Approximation 1")
+plot.plot([x for x in range(4, runs)], approx2_sums, label="Approximation 2")
+plot.plot([x for x in range(4, runs)], approx3_sums, label="Approximation 3")
 
-plot.title("Approximations vs Actual MVC (2)")
+plot.title("Expected Performance as Nodes Increase")
 plot.xlabel("Number of Nodes")
 plot.ylabel("Expected Performance")
 plot.legend()
